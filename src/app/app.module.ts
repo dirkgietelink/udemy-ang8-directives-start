cc35dad1c825d69b321230fa6653c1c16fa6c437
Dirk Gietelink <dirkgietelink@gmail.com>
2019-11-10 00:20:01 +0100

    vid 93: First Directive BasicHighlight, which has attribute selector with selector=[...],and ngOnInit() doing the magic.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-higlight/basic-higlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
